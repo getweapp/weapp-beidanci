a09482f611afafb2688647f3a581e91b615055af
getweapp <yw@getweapp.com>
2017-02-02 20:51:41 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,31 @@
+# 微信小程序－咩咩单词
+
+### 说明：
+
+实现背单词功能，特色：
+- 单词学习模块，包括单词拼写、音标、含义等
+- 单词查询模块
+- 词库选用英语专四、专八共计12347个单词
+
+### 数据接口:
+
+- https://api.getweapp.com/vendor/shanbay/bdc/search
+
+### 目录结构：
+
+- assets — 存放字体文件
+- images — 存放项目图片文件
+- pages — 存放项目页面文件
+- data — 存放单词数据文件
+
+### 开发环境：
+
+微信web开发者工具 v0.11.122100
+
+### 项目截图：
+
+https://www.getweapp.com/project?projectId=58932b1452e1e8733dc567f4
+
+### 感谢：
+
+本项目原始版本由airingursb提供：https://github.com/airingursb/miemie
---
+++ pages/settings/feedback/feedback.js
@@ -0,0 +1,55 @@
+// pages/settings/feedback/feedback.js
+
+
+var app = getApp()
+
+
+Page({
+  data: {
+    userInfo: {},
+    content: "",
+    connect: ""
+  },
+  onLoad: function (options) {
+
+    const that = this;
+    // 页面初始化 options为页面跳转所带来的参数
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  },
+  getContent: function (e) {
+    this.setData({
+      content: e.detail.value
+    })
+  },
+  getConnection: function (e) {
+    this.setData({
+      connect: e.detail.value
+    })
+  },
+  getNowFormatDate: function () {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+      month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+      strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+      + " " + date.getHours() + seperator2 + date.getMinutes()
+      + seperator2 + date.getSeconds();
+    return currentdate;
+  },
+  submitSuggestion: function () {
+ 
+  
+  }
+  
+})
